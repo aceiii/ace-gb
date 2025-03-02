9fe46bf9c2a849f5530a45e6f2d25dd6f0253e10
Borin Ouch <borin.ouch@gmail.com>
2025-03-02 01:29:56 -0800

    Clean up audio device switch statement

--- src/audio.cpp
+++ src/audio.cpp
@@ -13,46 +13,33 @@ uint8_t Audio::read8(uint16_t addr) const {
   switch (addr) {
     case std::to_underlying(IO::NR10):
       return ram[index] | 0b10000000;
-    case std::to_underlying(IO::NR11):
-      return ram[index];
-    case std::to_underlying(IO::NR12):
-      return ram[index];
-    case std::to_underlying(IO::NR13):
-      return ram[index];
-    case std::to_underlying(IO::NR14):
-      return ram[index];
-    case std::to_underlying(IO::NR21):
-      return ram[index];
-    case std::to_underlying(IO::NR22):
-      return ram[index];
-    case std::to_underlying(IO::NR23):
-      return ram[index];
-    case std::to_underlying(IO::NR24):
-      return ram[index];
     case std::to_underlying(IO::NR30):
       return ram[index] | 0b01111111;
-    case std::to_underlying(IO::NR31):
-      return ram[index];
     case std::to_underlying(IO::NR32):
       return ram[index] | 0b10011111;
-    case std::to_underlying(IO::NR33):
-      return ram[index];
     case std::to_underlying(IO::NR34):
       return ram[index] | 0b00111000;
     case std::to_underlying(IO::NR41):
       return ram[index] | 0b11000000;
-    case std::to_underlying(IO::NR42):
-      return ram[index];
-    case std::to_underlying(IO::NR43):
-      return ram[index];
     case std::to_underlying(IO::NR44):
       return ram[index] | 0b00111111;
-    case std::to_underlying(IO::NR50):
-      return ram[index];
-    case std::to_underlying(IO::NR51):
-      return ram[index];
     case std::to_underlying(IO::NR52):
       return ram[index] | 0b01110000;
+    case std::to_underlying(IO::NR11):
+    case std::to_underlying(IO::NR12):
+    case std::to_underlying(IO::NR13):
+    case std::to_underlying(IO::NR14):
+    case std::to_underlying(IO::NR21):
+    case std::to_underlying(IO::NR22):
+    case std::to_underlying(IO::NR23):
+    case std::to_underlying(IO::NR24):
+    case std::to_underlying(IO::NR31):
+    case std::to_underlying(IO::NR33):
+    case std::to_underlying(IO::NR42):
+    case std::to_underlying(IO::NR43):
+    case std::to_underlying(IO::NR50):
+    case std::to_underlying(IO::NR51):
+      return ram[index];
     default: return 0xff;
   }
 }
