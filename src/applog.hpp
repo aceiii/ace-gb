382fa16aacd8931ed56357dfc99b9211e4d4a67d
Borin Ouch <borin.ouch@gmail.com>
2026-02-16 19:29:34 -0800

    Add AppLog from chip8

---
+++ src/applog.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <imgui.h>
+#include <string>
+
+class AppLog {
+public:
+  AppLog();
+
+  void AddLog(const std::string &string);
+  void Clear();
+  void Draw();
+
+private:
+  ImGuiTextBuffer buffer_;
+  ImGuiTextFilter filter_;
+  ImVector<int> line_offsets_;
+  bool auto_scroll_;
+};
