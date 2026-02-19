e6f172e82a8d6f10a6bfc6da984b40dd4042c101
Borin Ouch <borin.ouch@gmail.com>
2026-02-18 20:31:30 -0800

    Fix naming/style for WramDevice

--- src/wram_device.h
+++ src/wram_device.h
@@ -5,6 +5,7 @@
 #include "mmu_device.hpp"
 
 class WramDevice : public MmuDevice {
+public:
   [[nodiscard]] bool IsValidFor(uint16_t addr) const override;
   void Write8(uint16_t addr, uint8_t byte) override;
   [[nodiscard]] uint8_t Read8(uint16_t addr) const override;
